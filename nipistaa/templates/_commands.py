411f5b79a397fba3bdebd2476897b24b6e7864b6
sigmanificient <edhyjox@gmail.com>
2022-01-14 05:09:31 +0100

    :sparkles: Added 2 new commands

--- nipistaa/templates/_commands.py
+++ nipistaa/templates/_commands.py
@@ -1,6 +1,17 @@
-async def ping():
+async def ping() -> str:
     return 'pong!'
 
+
+async def hello_world() -> str:
+    return 'Hello World!'
+
+
+async def say(message: str) -> str:
+    return message
+
+
 templates = {
-    'ping': ping
+    'ping': ping,
+    'hello_world': hello_world,
+    'say': say
 }
